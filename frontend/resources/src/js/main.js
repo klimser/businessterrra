9b212c005cebbc0ac57276f9c8bee82b954885aa
Sergey.Klimov <klimser2@gmail.com>
2018-11-22 18:49:31 +0300

    Decoration fix

--- frontend/resources/src/js/main.js
+++ frontend/resources/src/js/main.js
@@ -4,5 +4,45 @@ var Main = {
         var blockContent = '<div class="alert alert-dismissible ' + additionalClass + '"><button type="button" class="close" data-dismiss="alert" aria-label="Закрыть"><span>&times;</span></button>' + message + '</div>';
         if (append) $(blockSelector).append(blockContent);
         else $(blockSelector).html(blockContent);
+    },
+
+    scaleCaptcha: function() {
+        var captchaElements = $('.g-recaptcha');
+        if (captchaElements.length > 0) {
+            captchaElements.each(function() {
+                if ($(this).children().length > 0) {
+                    var reCaptchaDiv = $(this).children().first();
+                    var reCaptchaWidth = 304;
+                    var containerWidth = $(this).width();
+
+                    if (containerWidth > 0) {
+                        $(reCaptchaDiv).css(
+                            'transform',
+                            reCaptchaWidth > containerWidth ? 'scale(' + (containerWidth / reCaptchaWidth) + ')' : ''
+                        );
+                    }
+                }
+            });
+        }
     }
 };
+
+$(function() {
+    var captchaElements = $('.g-recaptcha');
+    if (captchaElements.length > 0) {
+        captchaElements.each(function() {
+            if ($(this).children().length > 0) {
+                Main.scaleCaptcha();
+            } else {
+                $(this).on('DOMSubtreeModified', function () {
+                    Main.scaleCaptcha();
+                });
+            }
+        });
+    }
+
+    $('.modal').on('shown.bs.modal', function () {
+        Main.scaleCaptcha();
+    });
+    $(window).resize(function() { Main.scaleCaptcha(); });
+});
